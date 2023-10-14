974747a9413c5fe1dfce934f831afc711cbabe69
CarJasper <cjasper@gmu.edu>
2023-10-14 10:35:38 -0400

    Exterior, Ceilings, Light fixes
    Built lighting for the major levels so it should be consistent

--- ChromaSpectra-HashTagCon/Assets/Scripts/FrontWallHide.cs
+++ ChromaSpectra-HashTagCon/Assets/Scripts/FrontWallHide.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class FrontWallHide : MonoBehaviour
 {
     public GameObject wall;
+    public GameObject roof;
     public GameObject doorFrame;
 
     Renderer render;
+    Renderer Roofrender;
     SpriteRenderer spRender;
     public void Start()
     {
         render = wall.GetComponent<Renderer>();
+        Roofrender = roof.GetComponent<Renderer>();
         spRender = doorFrame.GetComponent<SpriteRenderer>();
     }
     private void OnTriggerEnter(Collider other)
@@ -19,6 +22,7 @@ public class FrontWallHide : MonoBehaviour
         if(other.gameObject.tag == "Player") 
         { 
             render.enabled = false;
+            Roofrender.enabled = false;
             spRender.color = new Color(1f, 1f, 1f, 0.5f);
         }
         
@@ -29,6 +33,7 @@ public class FrontWallHide : MonoBehaviour
         if (other.gameObject.tag == "Player") 
         { 
             render.enabled = true;
+            Roofrender.enabled = true;
             spRender.color = new Color(1f, 1f, 1f, 1f);
         }
     }
